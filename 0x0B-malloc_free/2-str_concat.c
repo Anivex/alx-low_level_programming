dfafeb76206be9360d26c751a4a68a6ca0d57152
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-12 20:45:36 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,17 +11,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2, len;
+	int len1 = 0, len2 = 0, len;
 	char *s;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	for (len1 = 0; s1[len1] != '\0' && s1 == NULL; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0' && s2 == NULL; len2++)
-		;
-
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			;
+	}
+	
+	if (s2 != NULL)
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+			;
+	}
+	
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s == NULL)
 		return (NULL);
