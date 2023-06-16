d2319dc5e298a26466e14710ca2bfc5f90b106da
Anivex <mouad0abou@gmail.com>
2023-06-16 01:38:40 +0100

    1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * string_nconcat - function that concatenates two strings.
+ *
+ * @s1: first string.
+ * @s2: string concatenate with first string.
+ * @n: number of bytes.
+ *
+ * Return: a pointer to a newly allocated space.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0;
+	int len1, len2, j = 0;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2) > n ? n : _strlen(s2);
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s == NULL)
+		return (NULL);
+	/* check if s1 is no empty and fill s with s1*/
+	if (len1 > 0)
+	{
+		for (i = 0; i < len1; i++)
+			s[i] = s1[i];
+	}
+
+	if (len2 > 0)
+	{
+		while (s2[j] != '\0' && j < len2)
+		{
+			s[len1 + j] = s2[j];
+			j++;
+		}
+	}
+	s[len1 + len2] = '\0';
+
+	return (s);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * _strlen - count length of string passed.
+ *
+ * @str: string passe.
+ *
+ * Return: a number of length.
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	if (str == NULL || *str == '\0')
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
