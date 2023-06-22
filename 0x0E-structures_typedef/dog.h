77e949eefcfc705c49b45f116a374bbf2526df1a
Anivex <mouad0abou@gmail.com>
2023-06-22 14:39:36 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - structures of dog
+ *
+ * @name: name of dog.
+ * @age: dog's age
+ * @owner: owner's name of dog.
+ *
+ * Description: just structures contains name, age, owner name of dog.
+ */
+struct dog{
+
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
