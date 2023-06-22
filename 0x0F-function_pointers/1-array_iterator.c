7106ba5e2e9d18fb318574d293bd40119defe841
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-22 23:07:55 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function that executes a function given 
+ * array_iterator - function that executes a function given
  * as a parameter on each element of an array.
  *
  * @array: array of integer.
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 	{
-	
+
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
