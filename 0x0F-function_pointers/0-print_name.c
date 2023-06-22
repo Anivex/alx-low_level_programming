6da6f055ba8a880bc60fe706555bd33270a6ab67
Anivex <mouad0abou@gmail.com>
2023-06-22 18:29:11 +0100

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name: pointer to a char.
+ * @f: function pointer.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
