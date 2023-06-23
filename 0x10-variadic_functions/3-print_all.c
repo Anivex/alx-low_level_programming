73b5709b6ca3e86b6af85265bafe73c5eb81ea9b
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-23 22:14:09 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				s = va_arg(ap, char *);
 				printf("%s", s ? s : "(nil)");
-				printf("%s", format[i + 1] == '\0' ? "" : ", ");
+				printf("%s", format[i + 1] != '\0' && s ? ", " : "");
 				break;
 		}
 
