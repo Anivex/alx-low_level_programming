b1650d5a862b6279caf99cebfb12b312d51ba610
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-23 16:54:33 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%i", va_arg(ap, int));
 		if (i != n - 1)
-			printf("%s", separator);
+			printf("%s", separator ? separator : "");
 	}
 	printf("\n");
 	va_end(ap);
