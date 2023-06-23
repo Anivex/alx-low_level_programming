51fc0045daa0f7906dfd85898c1034bd920ecc71
Anivex <60237060+Anivex@users.noreply.github.com>
2023-06-23 22:28:02 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,32 +14,35 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 
-	while (format[i] != '\0')
+	if (format)
 	{
-		switch (format[i])
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%s%c", separator, va_arg(ap, int));
-				/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
-				break;
-			case 'i':
-				printf("%s%i", separator, va_arg(ap, int));
-				/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(ap, double));
-				/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
-				break;
-			case 's':
-				s = va_arg(ap, char*);
-				printf("%s%s", separator, s ? s : "(nil)");
-				/* printf("%s", format[i + 1] != '\0' && s ? ", " : ""); */
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(ap, int));
+					/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
+					break;
+				case 'i':
+					printf("%s%i", separator, va_arg(ap, int));
+					/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(ap, double));
+					/* printf("%s", format[i + 1] == '\0' ? "" : ", ");*/
+					break;
+				case 's':
+					s = va_arg(ap, char*);
+					printf("%s%s", separator, s ? s : "(nil)");
+					/* printf("%s", format[i + 1] != '\0' && s ? ", " : ""); */
+					break;
+			}
+			separator = ", ";
+			i++;
 		}
-		separator = ", ";
-		i++;
 	}
-
+	
 	printf("\n");
 	va_end(ap);
 }
