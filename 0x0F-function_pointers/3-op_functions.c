cca6338c7c20e9b6f3c6beb58fa6ba799ae0e574
Anivex <mouad0abou@gmail.com>
2023-06-23 01:17:04 +0100

    start task 4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addision of two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: the sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction of two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: the difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication of two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: the product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: the result of the division of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mul - Modulus of two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: the remainder of the division of a and b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
