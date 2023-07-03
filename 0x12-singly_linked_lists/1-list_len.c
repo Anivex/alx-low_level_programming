5d52a8ed85f7601695d4fd82cde0e3ae8c079770
Anivex <mouad0abou@gmail.com>
2023-07-03 17:55:03 +0100

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#inlcude "list.h"
+
+/**
+ * list_len - function calcule length of list.
+ *
+ * @h: head of single list.
+ *
+ * Return: the number of elements.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
