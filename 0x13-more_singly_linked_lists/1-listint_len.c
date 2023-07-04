49ecf068b75f8425d161d12b5ba1bd2aaa4d85c8
Anivex <mouad0abou@gmail.com>
2023-07-04 10:58:39 +0100

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ *
+ * @h: head of single linked list.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
