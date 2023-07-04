e88a5f165fbe29fe65e09122b8a262ebac1cfa62
Anivex <60237060+Anivex@users.noreply.github.com>
2023-07-04 22:38:25 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp, *h = *head;
 
-	if (h)
+
+	while (h)
 	{
-		while (h)
-		{
-			tmp = h->next;
-			free(h);
-			h = tmp;
-		}
+		tmp = h->next;
+		free(h);
+		h = tmp;
 	}
 
-	free(head);
+
+	*head = NULL;
 }
